ab057ae4ad00ae52960e1a261f379d475c2a62f5
Kate <katelynj29@gmail.com>
2016-11-23 10:58:10 -0500

    add button working

--- public/js/selectors.js
+++ public/js/selectors.js
@@ -1,36 +1,49 @@
 $(document).ready(function initializeSelectors () {
 
-const hotelSelector = function () {
-  for (let i = 0; i < hotels.length; i++) {
-    const $newHotel = `${hotels[i].name}`;
-    const $newLi = $('<option></option>');
-    $newLi.append($newHotel);
-    $('#hotel-choices').append($newLi);
-  }
-};
+  const hotelSelector = function () {
+    for (let i = 0; i < hotels.length; i++) {
+      const $newHotel = `${hotels[i].name}`;
+      const $newLi = $('<option></option>');
+      $newLi.append($newHotel);
+      $('#hotel-choices').append($newLi);
+    }
+  };
 
-const restaurantSelector = function () {
-  for (let i = 0; i < restaurants.length; i++) {
-    const $newRestaurant = `${restaurants[i].name}`;
-    const $newLi = $('<option></option>');
-    $newLi.append($newRestaurant);
-    $('#restaurant-choices').append($newLi);
-  }
-};
+  const restaurantSelector = function () {
+    for (let i = 0; i < restaurants.length; i++) {
+      const $newRestaurant = `${restaurants[i].name}`;
+      const $newLi = $('<option></option>');
+      $newLi.append($newRestaurant);
+      $('#restaurant-choices').append($newLi);
+    }
+  };
 
-const activitySelector = function () {
-  for (let i = 0; i < activities.length; i++) {
-    const $newActivity = `${activities[i].name}`;
-    const $newLi = $('<option></option>');
-    $newLi.append($newActivity);
-    $('#activity-choices').append($newLi);
-  }
-};
+  const activitySelector = function () {
+    $.each(activities, function (index, value) {
+      const $newActivity = `${value.name}`;
+      const $newLi = $('<option></option>');
+      $newLi.append($newActivity);
+      $('#activity-choices').append($newLi);
+    });
+    //.each version
+  };
 
-hotelSelector();
-restaurantSelector();
-activitySelector();
+  hotelSelector();
+  restaurantSelector();
+  activitySelector();
 
-$('').on('click', function () {});
+  const newItineraryItem = function (name, category) {
+    $(`#${category}`).append(`<div class="itinerary-item">
+        <span class="title">${name}</span>
+        <button class="btn btn-xs btn-danger remove btn-circle">x</button>
+      </div>`);
+  };
+
+  $('.btn-primary').on('click', function () {
+    const $category = $(this).siblings('select').data().type;
+    const $id = `#${$category}-choices`;
+    const $optionName = $($id).val();
+    newItineraryItem($optionName, $category);
+  });
 
 });
